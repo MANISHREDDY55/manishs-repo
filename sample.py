3f13b808ba4466b9487c5704db714b6775ca5dba
MANISHREDDY55 <79414785+MANISHREDDY55@users.noreply.github.com>
2021-02-23 18:27:36 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
-from flask input Flask
+from flask import Flask
 app =Flask(__name__)
 @app.route('/')
 def sample():
-return"welcome to my world"
+  return"welcome to my world"
 if__name__=='__main__':
-app.run(debug=True)
+  app.run(debug=True)
